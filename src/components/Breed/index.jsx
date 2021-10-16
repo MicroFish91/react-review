dd59f44e2c40632fc1e2347c141e93b342bd7d30
Matthew Fisher <mwfisher91@gmail.com>
2021-10-16 15:39:47 -0700

    Dogs

--- src/components/Breed/index.jsx
+++ src/components/Breed/index.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+
 const Breed = ({ selectedBreed }) => {
-  return <div>{selectedBreed}</div>;
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(async () => {
+    const imageJSON = await fetch(
+      `https://dog.ceo/api/breed/${selectedBreed}/images/random`
+    );
+    const image = await imageJSON.json();
+
+    setDogImage(image.message);
+  }, [selectedBreed]);
+
+  return (
+    <div>
+      <h1>{selectedBreed}</h1>
+      <img src={dogImage} alt="" />
+    </div>
+  );
 };
 
 export default Breed;
